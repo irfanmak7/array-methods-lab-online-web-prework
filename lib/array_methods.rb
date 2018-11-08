e530339eb239d331c6a8eb76072a5bc2cce48403
Irfan Maknojia <irfan.mak7@gmail.com>
2018-11-08 04:02:35 +0000

    automatically backed up by learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,15 +3,17 @@ def using_include(array, element)
 end
 
 def using_sort(array)
-
+  array.sort!
+  array
 end
 
 def using_reverse(array)
-
+  array.reverse!
+  array
 end
 
 def using_first(array)
-
+  array.first
 end
 
 def using_last(array)
